47ed9833f5a4c70b6f34616a9bc5a45328ac8550
to1dev <admin@to1.dev>
2025-04-11 23:49:08 +0200

    update title and add emoji

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          👺{siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
