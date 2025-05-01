40ece14e800ebe310cb6d95c9b77762d444cabcf
to1dev <admin@to1.dev>
2025-05-01 07:28:00 +0200

    update social card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: "img/docusaurus-social-card.jpg",
+        image: "img/card.png",
         colorMode: {
             defaultMode: "light",
             disableSwitch: true,
